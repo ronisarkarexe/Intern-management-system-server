128c425d08523f294d6c1b8bc87cb496c124b55a
Roni Sarkar <ronichandrasarkar@gmail.com>
2024-02-11 20:29:18 +0530

    change

--- src/app/modules/admin/admin.service.mjs
+++ src/app/modules/admin/admin.service.mjs
@@ -1,39 +1,7 @@
-import mongoose from 'mongoose';
 import { Admin } from './admin.model.mjs';
-import { Department } from '../department/department.model.mjs';
 
 const createAdminDb = async (payload) => {
-  let result = null;
-  const session = await mongoose.startSession();
-  try {
-    session.startTransaction();
-    const existingAdmin = await Admin.findOne({
-      departmentId: { _id: payload.departmentId },
-    });
-
-    if (existingAdmin) {
-      throw new Error(
-        'An admin with this email already exists in the specified department.',
-      );
-    }
-
-    const department = await Department.findOne({ _id: payload.departmentId });
-    if (!department) {
-      throw new Error('Department is not found!');
-    }
-    result = await Admin.create(payload);
-    department.adminDetails.push({ adminId: result._id });
-    await department.save();
-    await session.commitTransaction();
-    await session.endSession();
-  } catch (error) {
-    await session.commitTransaction();
-    await session.endSession();
-  }
-
-  if (!result) {
-    throw new Error('Admin is not created!');
-  }
+  const result = await Admin.create(payload);
   return result;
 };
 
--- src/app/modules/department/department.router.mjs
+++ src/app/modules/department/department.router.mjs
@@ -7,4 +7,6 @@ router.get('/', DepartmentController.getAllData);
 
 router.delete('/:id', DepartmentController.deleteDepartment);
 
+router.post('/:id/assign-admin', DepartmentController.assignAdmin);
+
 export const DepartmentRouters = router;
